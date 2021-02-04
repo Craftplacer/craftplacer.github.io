f588e18cdb2505fc08d15d68380e4474ecf4575b
Craftplacer <craftplacer@outlook.de>
2021-02-04 09:48:50 +0100

    Fix comments script

--- assets/scripts/comments.js
+++ assets/scripts/comments.js
@@ -5,9 +5,20 @@ if (button == null) {
 } else {
     button.onclick = function () {
         var script = document.getElementById('comments-script');
-        script.nodeName = 'script';
 
+        var newScript = document.createElement("script");
+        newScript.async = true;
+        newScript.src = "https://comments.app/js/widget.js?2";
+        newScript.setAttribute("data-comments-app-website", "YTaA8W00");
+        newScript.setAttribute("data-limit", 5);
+        newScript.setAttribute("data-color", "FF809E");
+        newScript.setAttribute("data-dark", "1");
+        newScript.setAttribute("data-dislikes", 1);
+
+        script.parentNode.append(newScript);
         button.parentNode.removeChild(button);
+
+        console.log('Successfully added Telegram widget');
     }
 
     button.disabled = false;
