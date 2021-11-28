21ccb76d9e7cb2bba817df2d2b641ea1843187c0
Craftplacer <Craftplacer@users.noreply.github.com>
2021-11-28 10:34:24 +0000

    jekyll build from Action 61746a1a0b685fdeaee1d00430bba76fbad4dab7

---
+++ js/diary.js
@@ -0,0 +1,45 @@
+function crypt() {
+    if (document.getElementById('encrypt').checked) {
+        return encrypt();
+    } else {
+        return fetchDiary();
+    }
+}
+
+function fetchDiary() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'encrypted-content.html', true);
+
+    var container = document.getElementById('diary-container');
+    var password = document.getElementById('password').value;
+
+    xhr.onloadstart = function () { container.innerText = "Loading..."; };
+    xhr.onerror = function () { container.innerText = 'Decryption failed.'; };
+    
+    xhr.onload = function () {
+        var decrypted = CryptoJS.AES.decrypt(xhr.responseText, password);
+        container.innerHTML = decrypted.toString(CryptoJS.enc.Utf8);
+    };
+
+    xhr.send(null);
+    return false;
+}
+
+function encrypt() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'content.html', true);
+
+    var container = document.getElementById('diary-container');
+    var password = document.getElementById('password').value;
+
+    xhr.onloadstart = function () { container.innerText = "Loading..."; };
+    xhr.onerror = function () { container.innerText = 'Encryption failed.'; };
+    
+    xhr.onload = function () {
+        var encrypted = CryptoJS.AES.encrypt(xhr.responseText, password);
+        container.innerText = encrypted;
+    };
+
+    xhr.send(null);
+    return false;
+}
